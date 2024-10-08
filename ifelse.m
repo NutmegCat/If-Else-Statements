f2df0195359436f51a0da9111c9723f7eaabcd33
NutmegCat <deltaechogames@gmail.com>
2024-10-08 10:46:43 -0400

    Part 2 Question

--- ifelse.m
+++ ifelse.m
@@ -1,1 +1,16 @@
-%EEEEEEEEEEEEE
+answer = questdlg('Would you like a dessert?', ...
+    'Dessert Menu', ...
+    "Ice cream","Cake","No thank you","No thank
+you");
+% Handle response
+switch answer
+    case "Ice cream"
+        disp([answer ' coming right up.'])
+        dessert = 1;
+    case "Cake"
+        disp([answer ' coming right up.'])
+        dessert = 2;
+    case "No thank you"
+        disp('I''ll bring you your cheque.')
+        dessert = 0;
+end
